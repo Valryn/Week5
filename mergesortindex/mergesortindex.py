25dcd43af4cd637ba59645cad07f4768c9a0a038
Nathan Lane <32247124+Valryn@users.noreply.github.com>
2019-11-14 20:01:17 -0800

    Add files via upload

--- mergesortindex/mergesortindex.py
+++ mergesortindex/mergesortindex.py
@@ -2,21 +2,32 @@ def mergeSort(alist, leftdex, rightdex):
 
     print("Splitting from " + str(leftdex) + " to " + str(rightdex))
 
-    if rightdex - leftdex > 1:
-        mid = (rightdex + leftdex) // 2
+    print(alist[leftdex:rightdex+1])
+
+    if rightdex - leftdex > 0:
+
+        mid = ((rightdex + leftdex) // 2)
+
+        print(mid)
+        print(leftdex)
+        print(rightdex)
+
         mergeSort(alist, leftdex, mid)
         mergeSort(alist, mid + 1, rightdex)
 
         i = 0
         j = 0
         k = 0
+        print(mid)
+        print(leftdex)
+        print(rightdex)
 
-        while i < (mid - leftdex) and j < (rightdex - mid):
-            if alist[leftdex + i] <= alist[mid + j]:
+        while i < (mid - leftdex + 1) and j < (rightdex - mid):
+            if alist[leftdex + i] <= alist[mid + j + 1]:
                 alist[leftdex + k] = alist[leftdex + i]
                 i = i + 1
             else:
-                alist[leftdex + k] = alist[mid + j]
+                alist[leftdex + k], alist[mid + j + 1] = alist[mid + j + 1], alist[leftdex + k]
                 j = j + 1
             k = k + 1
 
@@ -30,7 +41,9 @@ def mergeSort(alist, leftdex, rightdex):
             j = j + 1
             k = k + 1
     print("Merging from " + str(leftdex) + " to " + str(rightdex))
+    print(alist[leftdex:rightdex+1])
 
-alist = [54,26,93,17,77,31,44,55,20]
-mergeSort(alist, 0, len(alist))
+alist = [54,26,93,17]
+mergeSort(alist, 0, len(alist)-1)
 print(alist)
+
